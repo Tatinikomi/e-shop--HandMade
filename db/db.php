24b87225c08ae20b7d2c382a52adfd750be9e755
Tatinikomi <87527728+Tatinikomi@users.noreply.github.com>
2021-07-16 15:28:58 +0300

    Add files via upload

---
+++ db/db.php
@@ -0,0 +1,10 @@
+<?php 
+
+$host 	 	= "localhost";
+$user 	 	= "handmadf_hand";
+$pass 	 	= "gb8&lBxk";
+$db_name 	= "handmadf_hand";
+$connection = mysqli_connect($host, $user, $pass, $db_name);
+
+//Создаем подключение к БД
+?>
